e1262a976290214100f46358da329b13bc805ef3
Navnit03 <105769224+Navnit03@users.noreply.github.com>
2025-11-24 04:20:37 +0000

    Add files via upload

---
+++ glue_jobs/etl_forex/scripts/CustomerData-ingestion.py
@@ -0,0 +1,42 @@
+﻿import sys
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, DateType
+from pyspark.sql.functions import col, to_date, upper
+
+# Initializing Glue & Spark
+args = getResolvedOptions(sys.argv,
+                         ['RAW_SALES_PATH', 'PROCESSED_SALES_PATH','CURATED_SALES_PATH'])
+raw_sales_path = args['RAW_SALES_PATH']
+processed_sales_path = args['PROCESSED_SALES_PATH']
+curated_sales_path= args['CURATED_SALES_PATH']
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+# Defining Schema
+sales_schema = StructType([
+   StructField("customer_name", StringType(), True),
+   StructField("city", StringType(), True),
+   StructField("country", StringType(), True),
+   StructField("local_currency_amount", DoubleType(), True),
+   StructField("local_currency_code", StringType(), True),
+   StructField("sale_date",StringType(), True)
+])
+
+# Reading Data
+sales_df = spark.read.csv(raw_sales_path, schema=sales_schema, header=True)
+
+# Basic Transformations
+sales_df = sales_df.withColumn("local_currency_code", upper(col("local_currency_code")))
+
+sales_df = sales_df.withColumn("sale_date", to_date(col("sale_date"), "dd-MM-yyyy")) # date formatting
+
+final_df = sales_df.select("customer_name", "sale_date", "city", "country", "local_currency_code", "local_currency_amount")
+
+# Processed layer
+final_df.write.mode("overwrite").parquet(processed_sales_path)
+
+# Curated layer
+final_df.write.mode("overwrite").csv(curated_sales_path, header=True)
---
+++ glue_jobs/etl_forex/scripts/Transformation-and-DQChecks.py
@@ -0,0 +1,56 @@
+﻿import sys
+from datetime import datetime
+from pyspark.sql import SparkSession
+from awsglue.context import GlueContext
+
+from schemas.customer_schema import customer_schema
+from schemas.rate_schema import rate_schema
+from dq.dq_checks import apply_dq_checks
+from dq.transformations import join_customers_rates, split_valid_invalid, select_final_columns
+from awsglue.utils import getResolvedOptions
+
+# --- INIT ---
+spark = SparkSession.builder.appName("ForexDQJob").getOrCreate()
+glueContext = GlueContext(spark.sparkContext)
+
+# Define the parameters you expect
+args = getResolvedOptions(sys.argv, [
+    'customer_path',
+    'rate_path',
+    'output_path',
+    'curated_path',
+    'error_path'
+])
+
+customer_path = args['customer_path']
+rate_path     = args['rate_path']
+output_path   = args['output_path']
+curated_path  = args['curated_path']
+error_path    = args['error_path']
+
+# --- LOAD DATA ---
+customers_df = spark.read.option("header", True).schema(customer_schema).csv(customer_path)
+rates_df     = spark.read.option("header", True).schema(rate_schema).csv(rate_path)
+
+# --- TRANSFORM ---
+joined_df = join_customers_rates(customers_df, rates_df)
+dq_df = apply_dq_checks(joined_df)
+valid_df, invalid_df = split_valid_invalid(dq_df)
+
+# --- WRITE INVALID ---
+today_str = datetime.today().strftime("%Y-%m-%d")
+error_path = f"{error_path}{today_str}/"
+invalid_df.write.mode("overwrite").option("header", True).csv(error_path)
+
+# --- WRITE CLEANED ---
+clean_df = select_final_columns(valid_df)
+clean_df.write.mode("overwrite").parquet(output_path)
+clean_df.write.mode("overwrite").option("header", True).csv(curated_path)
+
+# --- DQ SCORE ---
+total_records = dq_df.count()
+valid_records = valid_df.count()
+dq_score = (valid_records / total_records) * 100 if total_records > 0 else 0
+print(f"Data Quality Score: {dq_score:.2f}%")
+
+spark.stop()
